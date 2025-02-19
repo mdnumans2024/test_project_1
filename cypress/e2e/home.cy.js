1de9fdc334bf0ff8d5e871f0f74c1d9ca1077ae0
mdnumans2024 <numanshorif2020@gmail.com>
2025-02-18 19:13:00 -0700

    Update 3

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,62 +1,78 @@
-//test suite
-describe('Login Page', () =>{
-  //pre-conditions. alternatively, we can use afterEach for post-conditions
-    beforeEach(() =>{
-
-      cy.visit('http://localhost:3000');
-    });
-
-
-    //Positive testing
-    it('should login with valid credentials', () =>{
-      //Arrange
-      cy.get('#email').type('test@example.com');
-      cy.get('#password').type('password123');
-
-      //Act
-      cy.get('#login-button').click();
-
-      //Assert
-      cy.on('window:alert', (txt) =>{
-        expect(txt).to.contains('Login Successful!')
-      });
-    });
-
-    //Negative testing
-    it('should display error message if wrong credentials', () =>{
-      //Arrange
-      cy.get('#email').type('test@examples.com');
-      cy.get('#password').type('password123');
-
-      //Act
-      cy.get('#login-button').click();
-
-      //Assert
-      cy.on('window:alert', (txt) =>{
-        expect(txt).to.contains('Invalid credentials')
-      });
+// Test suite
+describe('Login Page', () => {
+  // Pre-conditions. Alternatively, we can use afterEach for post-conditions.
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
   });
 
-  //Brute force testing
-  it('should for multiple failed login attempts', () =>{
- 
-    for(let i = 0; i < 5; i++){
-      cy.get('#email').clear().type('wrongexample@gmail.com')
-      cy.get('#password').clear().type('guessedpassword')
+  // Positive testing
+  it('should login with valid credentials', () => {
+    // Arrange
+    cy.get('#email').type('test@example.com');
+    cy.get('#password').type('password123');
+
+    // Act
+    cy.get('#login-button').click();
+
+    // Assert
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains('Login Successful!');
+    });
+  });
+
+  // Negative testing
+  it('should display an error message if wrong credentials are entered', () => {
+    // Arrange
+    cy.get('#email').type('test@examples.com');
+    cy.get('#password').type('password123');
+
+    // Act
+    cy.get('#login-button').click();
+
+    // Assert
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains('Invalid credentials');
+    });
+  });
+
+  // Brute force testing
+  it('should handle multiple failed login attempts', () => {
+    for (let i = 0; i < 5; i++) {
+      cy.get('#email').clear().type('wrongexample@gmail.com');
+      cy.get('#password').clear().type('guessedpassword');
       cy.get('#login-button').click();
     }
 
-    //Assert
-    cy.on('window:alert', (txt) =>{
-      expect(txt).to.contains('Invalid credentials')
+    // Assert
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains('Invalid credentials');
     });
   });
 
-   
-    it('should test whether the browser auto-fills the password', () =>{
-        cy.reload();
-        cy.get('#email').should('not.have.value');
-        cy.get('#password').should('not.have.value');
+  // Test against multiple form submissions
+  it('should prevent multiple form submissions', () => {
+    let alertText = '';
+
+    // Capturing alert messages into a variable
+    cy.on('window:alert', (txt) => {
+      alertText = txt;
     });
 
+    cy.get('#email').type('test@example.com');
+    cy.get('#password').type('password123');
+    cy.get('#login-button').click().then(() => {
+      expect(alertText).to.contains('Login Successful!');
+    });
+
+    cy.get('#login-button').click().then(() => {
+      expect(alertText).to.contains('You have submitted too many times!');
+    });
+  });
+
+  // Test whether the browser auto-fills the password
+  it('should test whether the browser auto-fills the password', () => {
+    cy.reload();
+    cy.get('#email').should('not.have.value');
+    cy.get('#password').should('not.have.value');
+  });
 });
